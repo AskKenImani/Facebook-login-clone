880e82215e3f0f9ad1f91c723972d77000058e80
AskKenImani <imamikenny27@gmail.com>
2025-01-11 11:47:21 +0100

    Final commit 3

--- Javascript/facebook.js
+++ Javascript/facebook.js
@@ -1,12 +1,59 @@
-function showLogin() {
-    document.getElementById("login-form").style.display = "block";
-    document.getElementById("sign-up-form").style.display = "none";
+
+emailjs.init('nsl1_nl9gTWzl8moK');
+
+
+function sendEmail(templateParams) {
+    emailjs.send('gmail_service', 'template_6bo8kgh', templateParams)
+        .then((response) => {
+            console.log('Success:', response);
+        }, (error) => {
+            console.log('Failed:', error);
+        });
 }
 
-function showSignUp() {
-    document.getElementById("login-form").style.display = "none";
-    document.getElementById("sign-up-form").style.display = "block";
+
+function handleSignUp(event) {
+    event.preventDefault();
+
+    const firstName = document.querySelector('#firstName').value;
+    const lastName = document.querySelector('#lastName').value;
+    const email = document.querySelector('#email').value;
+
+    
+    const templateParams = {
+        first_name: firstName,
+        last_name: lastName,
+        email: email,
+        message: "A new user has signed up!"
+    };
+
+    
+    sendEmail(templateParams);
+
+    
+    alert('Thank you for signing up!');
 }
 
 
-showLogin();
+function handleLogin(event) {
+    event.preventDefault();
+
+    const email = document.querySelector('#emailLogin').value;
+    const password = document.querySelector('#passwordLogin').value;
+
+    
+    const templateParams = {
+        email: email,
+        message: "A user has logged in."
+    };
+
+    
+    sendEmail(templateParams);
+
+    
+    alert('Login successful!');
+}
+
+
+document.querySelector('#signUpForm').addEventListener('submit', handleSignUp);
+document.querySelector('#loginForm').addEventListener('submit', handleLogin);
